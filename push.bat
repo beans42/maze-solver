141e2e94f199707923a0b86b478c30813a5bf93a
Ebrahim H <jizzware.xyz@gmail.com>
2020-12-25 06:13:44 -0500

    update

--- push.bat
+++ push.bat
@@ -5,6 +5,7 @@ git push https://github.com/beans42/maze-solve-web.git master
 
 cd out
 git init
+git branch web
 git add --all
 git commit -m "update"
 git push -f https://github.com/beans42/maze-solve-web.git web
