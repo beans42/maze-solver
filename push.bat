1fdada0b7403a35eb085daadcf62f829995a9b69
Ebrahim H <jizzware.xyz@gmail.com>
2020-12-26 18:38:50 -0500

    update

--- push.bat
+++ push.bat
@@ -3,10 +3,11 @@ git reset -- emsdk
 git commit -m "update"
 git push https://github.com/beans42/web-maze.git master
 
-cd out
+pushd out
 git init
 git checkout -b web
 git add --all
 git commit -m "update"
 git push -f https://github.com/beans42/web-maze.git web
 rm -rf .git
+popd
