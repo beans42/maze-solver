8cae221df05f073c2939d5b5dc6f9911689ff95d
Ebrahim H <jizzware.xyz@gmail.com>
2020-12-30 03:30:07 -0500

    update

--- push.bat
+++ push.bat
@@ -1,13 +1,13 @@
 git add --all
 git reset -- emsdk
 git commit -m "update"
-git push https://github.com/beans42/web-maze.git web
+git push https://github.com/beans42/maze-solver.git web
 
 pushd out
 git init
-git checkout -b web
+git checkout -b gh-pages
 git add --all
 git commit -m "update"
-git push -f https://github.com/beans42/web-maze.git gh-pages
+git push -f https://github.com/beans42/maze-solver.git gh-pages
 rm -rf .git
 popd
