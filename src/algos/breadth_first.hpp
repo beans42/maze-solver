bf16c2b6df530bf340cb46a75b715b13a8d9245c
Ebrahim H <jizzware.xyz@gmail.com>
2021-01-04 22:44:48 -0500

    BFS and DFS were named wrong way around, swapped them

--- src/algos/breadth_first.hpp
+++ src/algos/breadth_first.hpp
@@ -2,23 +2,23 @@
 #include "../includes.hpp"
 
 struct breadth_first : solution_interface {
-	ret_t solve(const maze_t& maze) {	
+	ret_t solve(const maze_t& maze) {
 		auto idx = [&maze](const point_t& p) { return p.y * maze.width + p.x; };
 
-		std::stack<point_t> stack;
+		std::queue<point_t> queue;
 		std::vector<point_t> previous(maze.width * maze.height, { -1, -1 });
 		std::vector<bool> visited(maze.width * maze.height, false);
 		std::vector<unsigned> distances(maze.width * maze.height, UINT_MAX);
 
-		stack.push(maze.start);
+		queue.push(maze.start);
 		distances[idx(maze.start)] = 0;
 		visited[idx(maze.start)] = true;
 
 		auto completed = false;
 
-		while (!stack.empty()) {
-			const auto current = stack.top();
-			stack.pop();
+		while (!queue.empty()) {
+			const auto current = queue.front();
+			queue.pop();
 
 			if (!maze.grid[idx(current)])
 				continue;
@@ -31,7 +31,7 @@ struct breadth_first : solution_interface {
 			for (const auto& v : current.neighbours(maze.width, maze.height)) {
 				if (!maze.grid[idx(v)]) continue;
 				if (!visited[idx(v)]) {
-					stack.push(v);
+					queue.push(v);
 					distances[idx(v)] = distances[idx(current)] + 1;
 					visited[idx(v)] = true;
 					previous[idx(v)] = current;
